a758d7f0104c5a4e53850bf7cee55988d7a88ba6
Krishna Kumar <55499842+krish7x@users.noreply.github.com>
2021-08-19 14:56:14 +0530

    dynamic routing

---
+++ pages/blog/[blogId].js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const Blogs = () => {
+  const router = useRouter();
+  const blogId = router.query.blogId;
+  return (
+    <div>
+      <h1>This is blog {blogId} </h1>
+    </div>
+  );
+};
+
+export default Blogs;
---
+++ pages/blog/others.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const others = () => {
+  return (
+    <div>
+      <h1>This is some other page</h1>
+    </div>
+  );
+};
+
+export default others;
