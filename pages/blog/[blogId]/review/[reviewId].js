95e218ba55e0fde40715b9b9278d57e5db545a71
Krishna Kumar <55499842+krish7x@users.noreply.github.com>
2021-08-19 15:10:33 +0530

    dynamic nested routes destructuring

--- pages/blog/[blogId]/review/[reviewId].js
+++ pages/blog/[blogId]/review/[reviewId].js
@@ -2,10 +2,12 @@ import { useRouter } from "next/router";
 
 const Review = () => {
   const router = useRouter();
-  const blogId = router.query.blogId;
+  const { blogId, reviewId } = router.query;
   return (
     <div>
-      <h1>This is review {blogId} </h1>
+      <h1>
+        This is blog {blogId} and its review is {reviewId}
+      </h1>
     </div>
   );
 };
