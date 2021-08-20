d788015bc953787a684900d334cb72b105a08627
Krishna Kumar <55499842+krish7x@users.noreply.github.com>
2021-08-20 16:54:05 +0530

    getStaticProps

---
+++ pages/users/index.js
@@ -0,0 +1,54 @@
+const Users = ({ users, todos }) => {
+  console.log(todos);
+  return (
+    <div>
+      <h1>User Component</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "80%",
+        }}>
+        <div
+          style={{
+            width: "50%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          {users?.map((data) => (
+            <p>{data?.name}</p>
+          ))}
+        </div>
+        <div
+          style={{
+            width: "50%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          {todos?.map((data) => (
+            <p>{data?.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
+
+export const getStaticProps = async () => {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const todos = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return {
+    props: {
+      users: await users.json(),
+      todos: await todos.json(),
+    },
+  };
+};
